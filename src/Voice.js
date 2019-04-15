ac4a48c305e08aae8a07ea88fd673016cbdb3552
Ronald Hajewski <ronald.hajewski@gmail.com>
2019-04-15 13:42:24 -0500

    pitch and rate working on multiple instances

--- src/Voice.js
+++ src/Voice.js
@@ -4,6 +4,9 @@ import React, { Component } from "react";
 
 class Voice extends Component {
   state = {
+    volume: 0.6,
+    pitch: 1,
+    rate: 1.2,
     text: ""
   };
 
@@ -13,11 +16,15 @@ class Voice extends Component {
 
   onSubmit = e => {
     let utterance = new SpeechSynthesisUtterance(this.state.text);
-    utterance.pitch = 2;
     utterance.volume = 0.5;
-    utterance.rate = 1.2;
+    utterance.pitch = this.state.pitch;
+    utterance.rate = this.state.rate;
     speechSynthesis.speak(utterance);
     e.preventDefault();
+  };
+
+  onInput = e => {
+    this.setState({ pitch: e.target.value, rate: e.target.value });
   };
 
   render() {
@@ -35,6 +42,33 @@ class Voice extends Component {
           />
           <br />
           <br />
+          <label for="pitch">Pitch: </label>
+
+          <input
+            name="pitch"
+            type="range"
+            min="0"
+            max="2"
+            step="0.1"
+            onInput={this.onInput}
+            defaultValue={this.state.pitch.value}
+          />
+          <br />
+          <br />
+
+          <label for="rate">Rate:</label>
+          <input
+            name="rate"
+            type="range"
+            min="0"
+            max="3"
+            step="0.1"
+            onInput={this.onInput}
+            defaultValue={this.state.rate.value}
+          />
+          <br />
+          <br />
+
           <button id="speak">speak</button>
         </form>
       </div>
