97fd1b7cbf1e887b9e9f88ff11f68b35e02d1ec8
Ronald Hajewski <ronald.hajewski@gmail.com>
2019-04-15 13:15:43 -0500

    the built-in web api seems to work best

--- src/Voice.js
+++ src/Voice.js
@@ -1,28 +1,42 @@
 import React, { Component } from "react";
-import { SayButton } from "react-say";
 
-speechSynthesis.speak(new SpeechSynthesisUtterance("Hey"));
+// speechSynthesis.speak(new SpeechSynthesisUtterance("Web Speech A P I"));
 
 class Voice extends Component {
+  state = {
+    text: ""
+  };
+
+  onChange = e => {
+    this.setState({ text: e.target.value });
+  };
+
+  onSubmit = e => {
+    let utterance = new SpeechSynthesisUtterance(this.state.text);
+    utterance.pitch = 2;
+    utterance.volume = 0.5;
+    utterance.rate = 1.2;
+    speechSynthesis.speak(utterance);
+    e.preventDefault();
+  };
+
   render() {
     return (
       <div>
         <h1>VOICE</h1>
         <br />
         <br />
-        <SayButton
-          onClick={event => console.log(event)}
-          speak="A quick brown fox jumped over the lazy dogs."
-        >
-          Tell me a story
-        </SayButton>{" "}
-        <SayButton
-          onClick={event => console.log(event)}
-          speak="Hello to all my friends."
-          voice={"v.lang"}
-        >
-          Hello
-        </SayButton>
+        <form onSubmit={this.onSubmit}>
+          <input
+            type="text"
+            name="text"
+            value={this.state.text}
+            onChange={this.onChange}
+          />
+          <br />
+          <br />
+          <button id="speak">speak</button>
+        </form>
       </div>
     );
   }
